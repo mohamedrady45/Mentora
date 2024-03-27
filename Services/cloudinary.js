724de193f3826556dee8203371bfde8a5fa79b98
Mai Abdallah <113540640+maiabdallah7@users.noreply.github.com>
2024-03-27 22:56:23 +0200

    Update cloudinary.js

--- Services/cloudinary.js
+++ Services/cloudinary.js
@@ -3,7 +3,7 @@ import {v2 as cloudinary} from 'cloudinary';
 cloudinary.config({ 
   cloud_name: 'di4ytdfwq', 
   api_key: '578268519668849', 
-  api_secret: '7RmDBIRP485evdUekhPHqgeKZpo' 
+  api_secret: '' 
 });
 
 module.exports = cloudinary;
