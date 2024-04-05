ba2c9a39d40a52912e5c7229d1e48a2b2eb641d7
Manar Khaled <manarkhaled2510@gmail.com>
2024-04-05 23:01:52 +0200

    chat updates

--- routers/chat.js
+++ routers/chat.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const chatController = require('../controllers/chatController2');
+const chatController = require('../controllers/chatController');
 const isAuth = require("../middlewares/isAuth");
 const upload=require('../middlewares/uploadFile')
 
