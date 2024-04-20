d4b84643e7acf274c5d1896738be560e45efafdb
Manar Khaled <manarkhaled2510@gmail.com>
2024-04-20 18:05:50 +0200

    add socket io to notification

--- Services/notification.js
+++ Services/notification.js
@@ -1,18 +1,29 @@
-const NotificationModel =require('../Models/notification')
+const NotificationModel = require('../Models/notification')
+const io = require('../socket').gitIO;
+
 
 class notificationService {
-   
-    static async addNotification(message,receiverIds,type)
-    {
-      const Notification = new NotificationModel({
-        users:receiverIds,
-        message:message,
-        type:type
-      });
-      return await Notification.save();;
-    }
+
+  static async addNotification(message, receiverIds, type) {
+    //add notification to database
+    const Notification = new NotificationModel({
+      users: receiverIds,
+      message: message,
+      type: type
+    });
+    await Notification.save();
+
+    //send notification to all users with socket io
+    receiverIds.forEach(userId => {
+      io.to(userId).emit('notification', Notification);
+    });
     
-    
+    return;
+  }
+
+
+
+
 }
-  
-  module.exports = notificationService;
+
+module.exports = notificationService;
