3145b45331d31020b448b4f2281e5e2fc55d61c4
Manar Khaled <manarkhaled2510@gmail.com>
2024-05-11 01:26:29 +0300

    edit isAuth middleware

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,26 +1,37 @@
-const JWT = require ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 const isAuth = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    
+    console.log(authHeader)
+
     if (!authHeader || !authHeader.startsWith('Bearer')) {
         return res.status(401).json({ message: 'You can\'t access this feature without logging in!' });
     }
 
     const token = authHeader.split(' ')[1];
- 
+    console.log(token)
     try {
-        const decodedToken = JWT.verify(token, process.env.SECRET_KEY);
-        if (!decodedToken) {
-            throw new Error('Invalid token');
-        }
+        // Read the public key
+        const cert = process.env.SECRET_KEY;
 
-        req.userId = decodedToken.userId;
-        next(); 
+        // Verify the token
+        jwt.verify(token, cert, { algorithms: ['HS256'] }, (err, payload) => {
+            if (err) {
+                console.error('Invalid signature:', err.message);
+                // Handle the error
+            } else {
+                // Token is valid, do something with the payload
+                console.log('Token payload:', payload);
+                req.userId = payload.userId;
+            }
+        });
+        
+        next();
     } catch (error) {
         console.error(error);
         return res.status(401).json({ message: 'Authentication failed' });
     }
 };
 
-module.exports =  isAuth;
+module.exports = isAuth;
