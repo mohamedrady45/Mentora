fe39a0395267e8d6387dd1b79125a1514223b29f
maiabdallah7 <maiabdalaa2019@gmail.com>
2024-05-19 19:03:44 +0300

    add createMentorRequest,     getMentorsRecommendation,     RequestRecommendedMentor,     MentorRejectRequest

--- controllers/MentorRequestController.js
+++ controllers/MentorRequestController.js
@@ -38,26 +38,90 @@ const createMentorRequest = async (req, res, next) => {
     }
 };
 
-
-const getRecommendedMentors = async (preferredLanguage, preferredGender, minSalary, maxSalary, interests, track , next) => {
+//get mentors recommendation
+const getMentorsRecommendation = async(req, res, next) =>{
     try {
         const mentors = await Mentor.find({
             languages: preferredLanguage,
             gender: preferredGender,
             minSalary: { $lte: maxSalary },
             maxSalary: { $gte: minSalary },
-            interests: { $in: interests },
             track: track
-        }).sort({ rating: -1 }).limit(10); 
+        }).select('_id firstName lastName profilePicture')
+          .sort({ rating: -1 }).limit(10); 
 
-        return mentors;
+        if (mentors.length === 0) {
+            return res.status(400).json({ message: 'No mentors match your request'});
+        }
+        res.status(400).json({ message: 'Mentors matches your request', mentors});
     } catch (error) {
         console.error('Error fetching recommended mentors:', error);
         throw new Error('Error fetching recommended mentors');
     }
 };
 
+// send the request to the mentor
+const RequestRecommendedMentor = async(req, res, next) =>{
+    try{
+       
+        const mentor = await Mentor.findById(req.userId);
+        if(!mentor){
+            return res.status(404).json({ message: 'There is no mentor selected' });   
+        }
+        const request = await Request.findById(req.params.id);
+        if(!request){
+            return res.status(404).json({ message: 'There is no request' });
+        }
+        mentor.requests.push(req.params.id);
+        await mentor.save();
+
+        res.status(200).json({ message: 'You requested the mentor.' });
+
+    }catch(err){
+        console.error('Error finding the mentor.', err);
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+//Mentor rejects the request
+const MentorRejectRequest = async(req, res, next) =>{
+    try{
+        const mentor = Mentor.findById(req.userId);
+        if (!mentor) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const {status} = req.body;
+
+        const request = await Request.findById(req.params.id);
+        if(!request){
+            return res.status(404).json({ message: 'There is no request' });
+        }
+        console.log(request);
+        //mentor rejects the request
+        if(status === "rejected"){
+            mentor.requests = mentor.requests.filter((requestId) => requestId !== req.params.id);
+            request.status = 'rejected';
+        } 
+        await mentor.save();
+        await request.save();
+        res.status(200).json({ message: 'You rejected the request.' });       
+    } catch(err){
+        console.error('Error rejecting the request.', err);
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
 
 module.exports = {
-    createMentorRequest
+    createMentorRequest,
+    getMentorsRecommendation,
+    RequestRecommendedMentor,
+    MentorRejectRequest
 };
