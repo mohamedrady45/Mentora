94a02e9bfa7a238a969c83301f83638045b03676
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-06-20 05:54:48 +0300

    fix error

--- controllers/communityController.js
+++ controllers/communityController.js
@@ -4,6 +4,15 @@ const Community  =  require('../Models/Community');
 const User = require('../Models/user');
 const  Question =  require('../Models/questions');
 const  Answer =  require('../Models/Answer');
+const getAllCommunities = async (req, res) => {
+  try {
+      const communities = await Community.find();
+      res.status(200).json({ communities });
+  } catch (error) {
+      console.error('Error fetching communities:', error);
+      res.status(500).json({ message: 'Error fetching communities' });
+  }
+};
 const createCommunity = async (req, res) =>{
     const { name, description  , track } = req.body;
   
@@ -291,5 +300,6 @@ const createCommunity = async (req, res) =>{
         getCommunityQuestions,
         getOneCommunityQuestion , 
         getCommunity,
-        searchCommunity
+        searchCommunity,
+        getAllCommunities
     };
