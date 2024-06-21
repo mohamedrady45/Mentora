63387dbf6b4cd7b2fadd6d1132ea60a2ca73fd89
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-06-21 19:53:38 +0300

    test

--- controllers/TrainingController.js
+++ controllers/TrainingController.js
@@ -59,7 +59,7 @@ const createTraining = async (req, res, next) => {
         const { name, description, track, level, requirements, Salary, duration, numberOfRequiredMentees, language } = req.body;
         const file =req.file;
         let attachment=null
-        if(!name||!description||!track||!leavel){
+        if(!name||!description||!track||!level){
             return res.status(400).json({message: 'Please fill all fields'})
         }
 
